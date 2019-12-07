d88167b99604dd73e88ba98c9aaca814204c53b0
rsalinas6721 <47551022+rsalinas6721@users.noreply.github.com>
2019-12-07 02:06:54 -0500

    Update keygen.c

--- keygen.c
+++ keygen.c
@@ -5,21 +5,21 @@
 #include <string.h>
 
 int main(int argc, char *argv[]){
-    srand(time(NULL)); // Time is seeded
+    srand(time(NULL));
     if (argc != 2) {
         printf("An Error Occured! Not Enough Parameters.\n");
         exit(0);
     }
     int keySize;
     char randomLetter;
-    keySize = atoi(argv[1]);    // Keysize is converted to integer
-    char key[keySize+1];    // char key array is initialized using keySize
+    keySize = atoi(argv[1]);
+    char key[keySize+1];
     int i;
-    for(i = 0; i < keySize; i++){   // For loop is used to calculate keySize
+    for(i = 0; i < keySize; i++){
         randomLetter = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"[rand()%27];
-        key[i] = randomLetter;  // Random letter is taken and added to key
+        key[i] = randomLetter;
     }
-    key[keySize] = '\0';    // Key adds '\0' to the end
-    printf("%s\n", key);    // Key is printed
+    key[keySize] = '\0';
+    printf("%s\n", key);
 return 0;
 }
